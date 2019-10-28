be3378e7b75bd1f9172e0810c9f7ecb5fcdb7659
Stefan Bischof <bischof.stefan@siemens.com>
2019-10-28 15:03:06 +0100

    add status monitorreplace tslint by eslint

--- server/common/server.ts
+++ server/common/server.ts
@@ -16,6 +16,7 @@ export default class ExpressServer {
   constructor() {
     const root = path.normalize(__dirname + '/../..');
     app.set('appPath', root + 'client');
+    app.use(require('express-status-monitor')())
     app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(cookieParser(process.env.SESSION_SECRET));
