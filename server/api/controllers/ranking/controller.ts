18b5c6fcc28e5a09d6a9334fc35db55a0d74f589
Stefan Bischof <bischof.stefan@siemens.com>
2020-12-16 10:05:06 +0100

    add error responses #7

--- server/api/controllers/ranking/controller.ts
+++ server/api/controllers/ranking/controller.ts
@@ -1,30 +1,31 @@
-/* 
+/*
  *  Copyright 2020 Siemens AG Oesterreich
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  
+ *
  *  SPDX-License-Identifier: Apache-2.0
- *  
+ *
  */
 
-import { RankingService } from '../../services/ranking.service';
-import { Request, Response } from 'express';
-import L from '../../../common/logger';
+import { RankingService } from "../../services/ranking.service";
+import { Request, Response } from "express";
+import L from "../../../common/logger";
+import { StatusCodes } from "http-status-codes";
 
 function weightedSum(values: object, weights: object): number {
   return Object.keys(weights)
-    .filter(key => values[key]) // For now: ignore if keys are missing from input
-    .map(key => weights[key]*values[key].value)
+    .filter((key) => values[key]) // For now: ignore if keys are missing from input
+    .map((key) => weights[key] * values[key].value)
     .reduce((sum, num) => sum + num, 0);
 }
 
@@ -32,37 +33,53 @@ export class Controller {
   private rankingService: RankingService;
 
   constructor(url: string) {
-    L.debug('Constructing controller', url);
+    L.debug("Constructing controller", url);
     this.rankingService = new RankingService(url);
   }
-  // // TODO remove
-  // all(req: Request, res: Response): void {
-  //   RankingService.all().then(r => res.json(r));
-  // }
-
-  // // TODO remove
-  // byId(req: Request, res: Response): void {
-  //   const id = Number.parseInt(req.params['id'])
-  //   RankingService.byId(id).then(r => {
-  //     if (r) res.json(r);
-  //     else res.status(404).end();
-  //   });
-  // }
-
-  // // TODO remove
-  // create(req: Request, res: Response): void {
-  //   RankingService.create(req.body.name).then(r =>
-  //     res
-  //       .status(201)
-  //       .location(`/api/v1/examples/${r.id}`)
-  //       .json(r),
-  //   );
-  // }
 
   public near(req: Request, res: Response): void {
-    
-    L.debug(`Controller.near: Calling rankingService.near(${JSON.stringify(req.query)})`);
-    this.rankingService.rank(req.query, req.headers)
-      .then(r => res.status(200).json(r));
+    L.debug(
+      `Controller.near: Calling rankingService.near(${JSON.stringify(
+        req.query
+      )})`
+    );
+    this.rankingService
+      .rank(req.query, req.headers)
+      .then((r) => res.status(StatusCodes.OK).json(r))
+      .catch((error) => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          L.error(
+            `Forwarding request to the broker returned an error: ${error.response.status}`
+          );
+          // Forward response from broker
+          res.status(error.response.status).send(error.response.data);
+          for (const header in error.response.headers) {
+            if (header === "transfer-encoding") {
+              continue;
+            }
+            res.setHeader(header, error.response.headers[header]);
+          }
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of http.ClientRequest
+          L.error("Timeout: no response received from the broker");
+          res.status(StatusCodes.GATEWAY_TIMEOUT);
+          res.json({
+            type: "https://uri.etsi.org/ngsi-ld/errors/InternalError",
+            title: "Timeout",
+            detail: "No response received from the broker",
+          });
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          L.error("Request failed: Error during setup of request to broker");
+          res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            type: "https://uri.etsi.org/ngsi-ld/errors/InternalError",
+            title: "Request failed",
+            detail: "Error during setup of request to broker",
+          });
+        }
+      });
   }
 }
--- server/common/indexClient.ts
+++ server/common/indexClient.ts
@@ -1,72 +1,58 @@
-/* 
+/*
  *  Copyright 2020 Siemens AG Oesterreich
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  
+ *
  *  SPDX-License-Identifier: Apache-2.0
- *  
+ *
  */
 
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { CREATED, NO_CONTENT, OK } from 'http-status-codes';
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
-import L from '../common/logger'
+import L from "../common/logger";
 // import { env } from '../validateEnv';
 
 export class IndexClient {
-    private readonly client: AxiosInstance;
-    private readonly contentType: AxiosRequestConfig = { headers: { 'Content-Type': 'application/json' } };
-    
-    constructor(indexUrl: string) {
-        this.client = axios.create({
-            baseURL: indexUrl,
-            headers: {
-                'Accept': 'application/ld+json'
-            }
-        });
-    }
-    
-    public async getEntities(params: object, headers: object): Promise<object[]> {
-        L.debug(`Calling Indexer params: ${JSON.stringify(params)} headers: ${JSON.stringify(headers)}`);
-        try {
-            const result = await this.client.get<object[]>(`/ngsi-ld/v1/entities`, {
-                params: params,
-                headers: headers
-            });
-            return result.data;
-        } catch (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                L.error("E1" + error.response.data);
-                L.error(error.response.status);
-                L.error(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
-                L.error("E2" + error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                L.error('E3', error.message);
-            }
-            L.error("E4" + error.config);
-        }
+  private readonly client: AxiosInstance;
+  private readonly contentType: AxiosRequestConfig = {
+    headers: { "Content-Type": "application/json" },
+  };
 
-        // if (result.status !== OK) {
-        //     
-        // }
+  constructor(indexUrl: string) {
+    L.debug(`NGSI-LD broker URL: ${indexUrl}`);
+    this.client = axios.create({
+      baseURL: indexUrl,
+      headers: {
+        Accept: "application/ld+json",
+      },
+    });
+  }
 
-        
-    }
+  public async getEntities(params: object, headers: object): Promise<object[]> {
+    L.debug(
+      `Calling Broker params: ${JSON.stringify(
+        params
+      )} headers: ${JSON.stringify(headers)}`
+    );
+    const result = await this.client.get<object[]>(`/ngsi-ld/v1/entities`, {
+      params: params,
+      headers: headers,
+    });
+
+    // if (result.status !== OK) {
+    //  Promise.reject()
+    // }
+
+    return result.data;
+  }
 }
